1038c16ac5b6437e2ab943d5063b59da375f0db4
Eduardo <egr.contato@gmail.com>
2021-12-11 10:56:55 -0400

    feat: adding initial settings to the BDDdesck app

---
+++ BDDesck/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for BDDesck project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BDDesck.settings')
+
+application = get_asgi_application()
